1de73941ac077b735d10b733226910d6ff1b032d
nam_dau_khac <2051052085nam@ou.edu.vn>
2024-03-30 16:13:42 +0700

    'aloalo'

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -75,10 +75,16 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'appdb',
+        'USER': 'root',
+        'PASSWORD': 'Admin@123',
+        'HOST': ''
     }
 }
+
+import pymysql
+pymysql.install_as_MySQLdb()
 
 
 # Password validation
