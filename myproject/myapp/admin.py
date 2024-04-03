a5c0135cb17649ff2caa46e111f43d6d3ddbfc51
Nga <hqnga1604@gmail.com>
2024-04-03 20:16:09 +0700

    add model

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -6,6 +6,8 @@ from django.utils.html import mark_safe
 from django.contrib.auth.models import Permission
 from ckeditor_uploader.widgets import CKEditorUploadingWidget
 
+from myapp.models import Location, BusRoute, User, Bus, BusSchedule, Booking, Seat, CustomerReview
+
 
 class AppAdminSite(admin.AdminSite):
     site_header = "Nam Dau Khac"
@@ -21,16 +23,51 @@ class AppAdminSite(admin.AdminSite):
     #         'stats': stats
     #     })
 
-# Register your models here.
 
-# class CategoryAdmin(admin.ModelAdmin):
-#     list_display = ['id', 'name']
-#     list_filter = ['id', 'name']
-#     search_fields = ['name']
 
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+
+class BusRouteAdmin(admin.ModelAdmin):
+    list_display = ['id', 'departure_point', 'arrival_point', 'is_active', 'price', 'estimated_travel_time']
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'avatar', 'user_type']
+
+
+class BusAdmin(admin.ModelAdmin):
+    list_display = ['id', 'license_plate', 'total_seats']
+
+
+class BusScheduleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'bus', 'bus_route', 'assigned_driver', 'departure_date', 'departure_time', 'surcharge',
+                    'number_of_seats_booked']
+
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ['id', 'bus_schedule', 'sales_staff','customer', 'is_active', 'created_at', 'total_seats']
+
+
+class SeatAdmin(admin.ModelAdmin):
+    list_display = ['id', 'number_seat', 'booking']
+
+
+class CustomerReviewAdmin(admin.ModelAdmin):
+    list_display = ['id', 'reviewed_trip', 'customer', 'review_text', 'rating', 'created_at']
 
 
 admin_site = AppAdminSite(name="myapp")
 
-# admin_site.register(Category,CategoryAdmin)
+admin_site.register(Location, LocationAdmin)
+admin_site.register(BusRoute, BusRouteAdmin)
+admin_site.register(User, UserAdmin)
+admin_site.register(Bus, BusAdmin)
+admin_site.register(BusSchedule, BusScheduleAdmin)
+admin_site.register(Booking, BookingAdmin)
+admin_site.register(Seat, SeatAdmin)
+admin_site.register(CustomerReview, CustomerReviewAdmin)
+
 admin_site.register(Permission)
--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -6,6 +6,7 @@ from cloudinary.models import CloudinaryField
 
 # Create your models here.
 from django.db.transaction import on_commit
+from rest_framework.exceptions import ValidationError
 
 
 class BaseModel(models.Model):
@@ -17,27 +18,8 @@ class BaseModel(models.Model):
 class Location(BaseModel):
     name = models.CharField(max_length=50)
 
-
-# class Category(BaseModel):
-#     name = models.CharField(max_length=50)
-#
-#     def __str__(self):
-#         return self.name
-
-
-class BusRoute(models.Model):
-    departure_point = models.ForeignKey(Location, on_delete=models.CASCADE, related_name="departure_buses")
-    arrival_point = models.ForeignKey(Location, on_delete=models.CASCADE, related_name="arrival_buses")
-    is_active = models.BooleanField(default=True)  # Clearer variable name
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    departure_time = models.TimeField(blank=True, null=True)
-
     def __str__(self):
-        return f"{self.departure_point} -> {self.arrival_point} (Price: ${self.price:.2f})"
-
-    class Meta:
-        verbose_name = "Bus Route"
-        verbose_name_plural = "Bus Routes"
+        return self.name
 
 
 class User(AbstractUser):
@@ -50,46 +32,97 @@ class User(AbstractUser):
     ), default='customer')
 
 
-# chuyen xe
-class BusSchedule(models.Model):
+class Bus(models.Model):
+    license_plate = models.CharField(max_length=50, unique=True)
+    total_seats = models.PositiveIntegerField(default=40)
 
-    bus = models.ForeignKey(BusRoute, on_delete=models.CASCADE, related_name="schedules")
+    def __str__(self):
+        return f"{self.license_plate}"
+
+
+class BusRoute(models.Model):
+    departure_point = models.ForeignKey(Location, on_delete=models.CASCADE, related_name="departure_buses")
+    arrival_point = models.ForeignKey(Location, on_delete=models.CASCADE, related_name="arrival_buses")
+    is_active = models.BooleanField(default=True)  # Clearer variable name
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    estimated_travel_time = models.PositiveIntegerField(blank=True, null=True)
+
+    def clean_fields(self, *args, **kwargs):
+        if self.departure_point == self.arrival_point:
+            raise ValidationError("Departure and arrival points cannot be the same.")
+
+        super().clean_fields()
+
+    def __str__(self):
+        return f"{self.departure_point} -> {self.arrival_point} "
+
+    class Meta:
+        verbose_name = "Bus Route"
+        verbose_name_plural = "Bus Routes"
+        unique_together = (("departure_point", "arrival_point"),)
+
+    def get_price(self):
+        return self.price
+
+
+
+class BusSchedule(models.Model):
+    bus = models.ForeignKey(Bus, on_delete=models.CASCADE, related_name="bus_schedules")
+    bus_route = models.ForeignKey(BusRoute, on_delete=models.CASCADE, related_name="route_schedules")
     assigned_driver = models.ForeignKey(User, on_delete=models.SET_NULL, null=True,
                                         related_name="assigned_schedules")
     departure_date = models.DateField()
     departure_time = models.TimeField()
-    price_per_seat = models.DecimalField(max_digits=10, decimal_places=2)
+    surcharge = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+    number_of_seats_booked = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.bus}: {self.bus_route}"
 
     class Meta:
         unique_together = (("bus", "departure_date", "departure_time"),)
 
-    def __str__(self):
-        return f"{self.bus}: {self.departure_date} {self.departure_time} ({self.assigned_driver} - ${self.price_per_seat:.2f})"  # Informative string representation
+    def is_seat_available(self):
+        return self.bus.get_seats() > self.number_of_seats_booked
+
+    def book_seat(self):
+        if self.is_seat_available():
+            self.number_of_seats_booked += 1
+            self.save()
+            return True
+        return False
+
+    def cancel_seat_booking(self):
+        if self.number_of_seats_booked > 0:
+            self.number_of_seats_booked -= 1
+            self.save()
+            return True
+        return False
+
+    def get_price(self):
+        return self.bus_route.get_price() + self.surcharge
 
 
-# ve xe
 
 class Booking(models.Model):
-    scheduled_trip = models.ForeignKey(BusSchedule, on_delete=models.CASCADE,
-                                       related_name="bookings")
+    bus_schedule = models.ForeignKey(BusSchedule, on_delete=models.CASCADE,
+                                     related_name="bookings")
+    sales_staff = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name="sales_bookings")
     customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bookings")
-    number_of_seats = models.PositiveIntegerField(
-        validators=[MaxValueValidator(10)])
-    total_price = models.DecimalField(max_digits=10, decimal_places=2)
-    booking_date = models.DateField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)  # Clearer variable name
+    created_at = models.DateTimeField(auto_now_add=True)
+    total_seats = models.PositiveIntegerField(default=1)
 
-    def __str__(self):
-        return f"{self.customer} - {self.scheduled_trip} ({self.number_of_seats} seats - ${self.total_price:.2f})"
+
+class Seat(models.Model):
+    number_seat = models.IntegerField()
+    booking = models.ForeignKey(Booking, on_delete=models.CASCADE, related_name="seats")
 
 
 class CustomerReview(models.Model):
-
     reviewed_trip = models.ForeignKey(BusSchedule, on_delete=models.CASCADE,
                                       related_name="reviews")
     customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reviews")
     review_text = models.TextField()
     rating = models.IntegerField(choices=((1, "Poor"), (2, "Fair"), (3, "Good"), (4, "Very Good"), (5, "Excellent")))
     created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.customer} reviewed {self.reviewed_trip} ({self.rating}): {self.review_text[:50]}..."
